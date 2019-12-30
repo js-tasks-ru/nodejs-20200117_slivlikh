cea53bc4801ebdcaf8358ec0558b28ac093c66e6
Slava <slivlikh@gmail.com>
2019-12-31 01:59:55 +0200

    add solution for 2-module 2-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,19 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.sizeLimit = options.limit;
+    this.streamContentLength = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    const chunkString = chunk.toString();
+    this.streamContentLength += chunkString.length;
+    if (this.streamContentLength <= this.sizeLimit) {
+      this.push(chunk);
+      callback(null);
+      return;
+    }
+    callback(new LimitExceededError());
   }
 }
 
--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -1,15 +1,51 @@
 const stream = require('stream');
 const os = require('os');
 
+const isLastIndex = function(currentIndex, arr) {
+  return currentIndex === arr.length - 1;
+};
+const getLastChar = function(string) {
+  return string[string.length - 1];
+};
+const isLineBreakChar = function(char) {
+  return char === os.EOL;
+};
+
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.tail = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const string = this.tail + chunk.toString();
+    if (string.indexOf(os.EOL) === -1) {
+      this.tail = string;
+      callback(null);
+      return;
+    }
+    const splitted = string.split(os.EOL);
+    splitted.forEach((line, index) => {
+      if (!isLastIndex(index, splitted)) {
+        this.push(line);
+        return;
+      }
+
+      const lastChar = getLastChar(string);
+      if (isLineBreakChar(lastChar)) {
+        this.push(line);
+        callback(null);
+        return;
+      }
+
+      this.tail = line;
+      callback(null);
+    });
   }
 
   _flush(callback) {
+    this.push(this.tail);
+    callback();
   }
 }
 
