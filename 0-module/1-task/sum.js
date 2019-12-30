14b33180b866700f35c2699d5f5b62ab50681b14
Slava <slivlikh@gmail.com>
2019-12-31 00:03:42 +0200

    add solution for 1-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,14 @@
 function sum(a, b) {
-  /* ваш код */
+  if (
+    typeof a !== 'number' ||
+    Number.isNaN(a) ||
+    typeof b !== 'number' ||
+    Number.isNaN(b)
+  ) {
+    throw new TypeError();
+  }
+
+  return a + b;
 }
 
 module.exports = sum;
