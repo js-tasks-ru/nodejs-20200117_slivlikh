4f699175c67f00bd4a30d6bd4b32b3ca869a6a32
Slava <slivlikh@gmail.com>
2020-01-03 00:54:13 +0200

    4-module micro express

---
+++ 4-module/1-task/MicroRouter.js
@@ -0,0 +1,151 @@
+const url = require('url');
+const UrlPattern = require('url-pattern');
+
+const ReqMethods = {
+  GET: 'get',
+  POST: 'post',
+};
+
+
+class MicroRouter {
+  constructor() {
+    this.getController = new MethodController();
+    this.postController = new MethodController();
+  }
+
+  get(url, handler) {
+    this.getController.registerHandler(url, handler);
+  }
+
+  post(url, handler) {
+    this.postController.registerHandler(url, handler);
+  }
+
+  middleware() {
+    return (req, res, next) => {
+      const controller = this.resolveController(req);
+      controller.executeHandler(req, res, next);
+    };
+  }
+
+  resolveController(req) {
+    console.log(req.method);
+    switch (req.method.toLowerCase()) {
+      case ReqMethods.GET:
+        return this.getController;
+      case ReqMethods.POST:
+        return this.postController;
+      default:
+        console.warn(`There is not repo for ${req.method} method`);
+    }
+  }
+}
+
+
+class HandlersRepo {
+  constructor() {
+    this.handlersRepo = new Map();
+  }
+
+  addHandler(key, handler) {
+    const pattern = new UrlPattern(key);
+    this.handlersRepo.set(pattern, handler);
+  }
+  getHandler(key) {
+   return this.handlersRepo.get(key);
+  }
+
+  keys() {
+    return this.handlersRepo.keys();
+  }
+}
+
+class Selector {
+  constructor(repo) {
+    this.repo = repo;
+  }
+  selectHandlersByKey(key, params) {
+    const handlers = new Map();
+    Array.from(this.repo.keys()).forEach((pattern) => {
+      if (params.length === handlers.size) {
+        return;
+      }
+      const result = pattern.match(key);
+      if (result) {
+        handlers.set(this.repo.getHandler(pattern), result);
+      }
+    });
+
+    return handlers;
+  }
+}
+
+class MethodController {
+  constructor() {
+    this.handlersRepo = new HandlersRepo();
+    this.repoSelector = new Selector(this.handlersRepo);
+  }
+
+  registerHandler(url, handler) {
+    this.handlersRepo.addHandler(url, handler);
+  }
+
+  async executeHandler(req, res, next) {
+    const pathname = url.parse(req.url).pathname;
+    const handlersMap = this.repoSelector.selectHandlersByKey(pathname, {length: 1});
+    const handlers = Array.from(handlersMap.keys());
+
+    for (const handler of handlers) {
+      if (this.isSyncHandler(handler)) {
+        const error = this.executeSyncHandler(req, res, handler, handlersMap.get(handler));
+        if (error !== undefined) {
+          next(error);
+          return;
+        }
+      } else {
+        const error = await this.executeAsyncHandler(req, res, handler, handlersMap.get(handler));
+        if (error !== undefined) {
+          next(error);
+          return;
+        }
+      }
+    }
+    next();
+  }
+
+  executeAsyncHandler(req, res, handler, params) {
+    this.addParamsToRequest(req, params);
+
+    return new Promise((resolve) => {
+      try {
+        handler(req, res, (err) => {
+          if (!err) {
+            return resolve();
+          }
+          return resolve(err);
+        });
+      } catch (e) {
+        resolve(e);
+      }
+    });
+  }
+
+  executeSyncHandler(req, res, handler, params) {
+    this.addParamsToRequest(req, params);
+    try {
+      handler(req, res);
+    } catch (e) {
+      return e;
+    }
+  }
+
+  isSyncHandler(handler) {
+    return handler.length < 3;
+  }
+
+  addParamsToRequest(req, params) {
+    req.params = params;
+  }
+}
+
+module.exports = MicroRouter;
--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,23 +1,94 @@
-const url = require('url');
 const http = require('http');
-const path = require('path');
+const microExpress = require('./MicroExpress');
+const MicroRouter = require('./MicroRouter');
 
-const server = new http.Server();
+const app = microExpress();
+const router = new MicroRouter();
+const server = http.createServer(app.handler());
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.end('Home Page');
+// });
+app.use(router.middleware());
 
-server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
-
-  switch (req.method) {
-    case 'GET':
-
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.statusCode = 500;
+  res.end(JSON.stringify(err));
 });
 
-module.exports = server;
+router.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.end('Home Page 1');
+});
+
+router.get('/about', (req, res, next) => {
+  setTimeout(() => {
+    res.statusCode = 200;
+    res.end('About Page');
+    next();
+  }, 2000);
+});
+
+router.get('/product(/:id)', (req, res, next) => {
+  setTimeout(() => {
+    res.statusCode = 200;
+    res.end(`Product Page ${req.params.id}`);
+    next();
+  }, 2000);
+});
+
+router.get('(/:url)', (req, res, next) => {
+  setTimeout(() => {
+    res.statusCode = 200;
+    res.end(`First ${req.params.url}`);
+    next();
+  }, 100);
+});
+
+router.get('(/:url)/*', (req, res, next) => {
+  setTimeout(() => {
+    res.statusCode = 501;
+    res.end(`Deep url ${req.params.url} 501`);
+    next();
+  }, 100);
+});
+
+server.listen(3000, () => {
+  console.log('server is listening on 3000 port');
+});
+
+
+process.on('error', (err) => {
+  // console.log(err);
+});
+// const url = require('url');
+// const http = require('http');
+// const path = require('path');
+// const stream = require('stream');
+// const server = new http.Server();
+//
+//
+//
+// server.on('request', (req, res) => {
+//   const pathname = url.parse(req.url).pathname.slice(1);
+//
+//   const filepath = path.join(__dirname, 'files', pathname);
+//
+//   switch (req.method) {
+//     case 'GET':
+//
+//       break;
+//
+//     default:
+//       res.statusCode = 501;
+//       res.end('Not implemented');
+//   }
+// });
+//
+// server.on('error', (req, res) => {
+//   res.statusCode = 500;
+//   res.end('Internal server error 500');
+// });
+//
+// module.exports = server;
